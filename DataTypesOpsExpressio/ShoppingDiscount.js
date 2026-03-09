af45cc75c7cfbdf75572354e06dc5293812b4ae3
patilmanasi09 <patilmanasi151@gmail.com>
2026-03-09 22:09:01 +0530

    change code

--- DataTypesOpsExpressio/ShoppingDiscount.js
+++
@@ -1,9 +0,0 @@
-const prompt = require("prompt-sync")();
-
-let total = parseFloat(prompt("Enter total amount: "));
-
-if (total > 100){
-    total -= total * 0.10;
-}
-
-console.log("Discount total:",total);
--- DataTypesOpsExpressio/logicalOpe.js
+++
@@ -1,6 +0,0 @@
-const prompt = require("prompt-sync")();
-let mathScore = parseFloat(prompt("Enter your math score:"));
-let scienceScore = parseFloat(prompt("Enter your science score:"));
-
-let isEligible = (mathScore >= 90) && (scienceScore >= 85);
-console.log("Scholarship Eligibility:",  isEligible);
